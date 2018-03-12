4af2170304596e9daf3f606cbca7ede146ff5c86
Adam <adamf04@gmail.com>
2018-03-12 10:44:02 -0400

    conditional statements in custom API to retrieve custom post types and push them into a nested associative array

--- wp-content/themes/fictional-university-theme/includes/search-route.php
+++ wp-content/themes/fictional-university-theme/includes/search-route.php
@@ -37,9 +37,30 @@ function universitySearchResults($data) {
 				'permalink'	=>  get_the_permalink()
 			));	
 		}
-
+		// conditional check if post type is professor
 		if (get_post_type() == 'professor') {
 			array_push($results['professors'], array(
+				'title' 	=> get_the_title(),
+				'permalink'	=>  get_the_permalink()
+			));	
+		}
+
+		if (get_post_type() == 'program') {
+			array_push($results['programs'], array(
+				'title' 	=> get_the_title(),
+				'permalink'	=>  get_the_permalink()
+			));	
+		}
+
+		if (get_post_type() == 'campus') {
+			array_push($results['campuses'], array(
+				'title' 	=> get_the_title(),
+				'permalink'	=>  get_the_permalink()
+			));	
+		}
+
+		if (get_post_type() == 'event') {
+			array_push($results['events'], array(
 				'title' 	=> get_the_title(),
 				'permalink'	=>  get_the_permalink()
 			));	

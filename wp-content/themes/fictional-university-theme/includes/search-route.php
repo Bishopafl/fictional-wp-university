dab452d6d258ab9cc5eee79611cbaa2b063d62ef
Adam <adamf04@gmail.com>
2018-03-12 16:17:59 -0400

    Made search relationships dynamic
    Made the search overlay relationships dynamic, so if you search for math you will see math professors.  Also elminiated the potentiality of false positives if a math description has the word biology, WP will not use the default editor, but an editor I've created in the custom fields section.

--- wp-content/themes/fictional-university-theme/includes/search-route.php
+++ wp-content/themes/fictional-university-theme/includes/search-route.php
@@ -50,6 +50,7 @@ function universitySearchResults($data) {
 
 		if (get_post_type() == 'program') {
 			array_push($results['programs'], array(
+				'id'		=> get_the_id(),
 				'title' 	=> get_the_title(),
 				'permalink'	=>  get_the_permalink()
 			));	
@@ -83,32 +84,40 @@ function universitySearchResults($data) {
 
 	}
 
-	$programRelationshipQuery = new WP_Query(array(
-		'post_type'		=> 'professor',
-		'meta_query'	=> array(
-			array(
+	// code will only execute if programs relate to search
+	if ($results['programs']) {
+		$programsMetaQuery = array('relation' => 'OR');
+
+		foreach ($results['programs'] as $program) {
+			array_push($programsMetaQuery, array(
 				'key' => 'related_program', // advanced custom field name
 				'compare' => 'LIKE',
-				'value' => '"60"' 
-			)
-		)
-	));
-
-	while ($programRelationshipQuery->have_posts()) {
-		$programRelationshipQuery->the_post();
-
-		// conditional check if post type is professor based on the relationship query on line 86
-		if (get_post_type() == 'professor') {
-			array_push($results['professors'], array(
-				'title' 	=> get_the_title(),
-				'permalink'	=> get_the_permalink(),
-				'image'		=> get_the_post_thumbnail_url(0, 'professorLandscape')
-			));	
+				'value' => '"' . $program['id'] . '"'
+			));
 		}
-	}
 
-	// function that removes duplicates that could potentially get duplicate results 
-	$results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+		$programRelationshipQuery = new WP_Query(array(
+			'post_type'		=> 'professor',
+			'meta_query'	=> $programsMetaQuery
+		));
+
+		while ($programRelationshipQuery->have_posts()) {
+			$programRelationshipQuery->the_post();
+
+			// conditional check if post type is professor based on the relationship query on line 86
+			if (get_post_type() == 'professor') {
+				array_push($results['professors'], array(
+					'title' 	=> get_the_title(),
+					'permalink'	=> get_the_permalink(),
+					'image'		=> get_the_post_thumbnail_url(0, 'professorLandscape')
+				));	
+			}
+		}
+
+		// function that removes duplicates that could potentially get duplicate results 
+		$results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+
+	}
 
 	return $results;
 }

17c7d2b63969e54f43e49f3dbced0ea2d9e211b6
Adam <adamf04@gmail.com>
2018-04-25 16:03:16 -0400

    login information for users
    if the user is logged in as a subscriber, the admin bar is hidden and the user is redirected to the homepage, even if they try to type 'wp-admin' in the url

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -161,5 +161,35 @@ add_filter('acf/fields/google_map/api', 'universityMapKey');
 
 /*--------------------------------------------------------------------------------------------------------------------------------*/
 
+/*******************************************/
+/*		redirect subscriber accounts	   */
+/*		out of admin and onto homepage	   */
+/*******************************************/
+
+function redirectSubsToFrontend() {
+	// code redirects subscriber users to front end without having them view the admin dashboard in wp
+	$ourCurrentUser = wp_get_current_user();
+
+	if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+		wp_redirect(site_url('/'));
+		exit;
+	}
+}
+
+add_action('admin_init', 'redirectSubsToFrontend');
+
+
+
+function noSubsAdminBar() {
+	// code hides the admin bar if the user role is set to subscriber
+	$ourCurrentUser = wp_get_current_user();
+
+	if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+		show_admin_bar(false);
+	}
+}
+
+add_action('wp_loaded', 'noSubsAdminBar');
+
 
 ?>

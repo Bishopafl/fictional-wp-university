ececdd6c4a3f12f3e01ed6255c67e58096d78f1c
Adam <adamf04@gmail.com>
2018-04-26 23:18:44 -0400

    created custom post type for notes
    Next, will hook up the edit and delete buttons to REST API

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -70,6 +70,22 @@ function university_post_types() {
 		),
 		'menu_icon' => 'dashicons-welcome-learn-more'
 	));
+
+	// Note Post Type
+	register_post_type('note', array(
+		'show_in_rest'		=> true, // work with custom posttype from rest api
+		'supports' 			=> array('title', 'editor'),
+		'public' 			=> false, //notes should be private to users
+		'show_ui'			=> true, // shows custom post type in admin dashboard
+		'labels' 			=> array(
+			'name' 				=> 'Notes',
+			'add_new_item' 		=> 'Add New Note',
+			'edit_item'			=> 'Edit Note',
+			'all_items'			=> 'All Notes',
+			'singular_name'		=> 'Note'
+		),
+		'menu_icon' => 'dashicons-welcome-write-blog'
+	));
 }
 
 add_action('init', 'university_post_types');

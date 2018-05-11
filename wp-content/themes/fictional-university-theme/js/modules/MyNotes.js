9e9feb90bfcb2365921d596cc362abec617447b1
Adam <adamf04@gmail.com>
2018-05-11 16:15:52 -0400

    updated the note edit functionality to save from the front end to the back end using the wp rest api

--- wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -10,9 +10,37 @@ class MyNotes {
 	events() {
 		$(".delete-note").on("click", this.deleteNote);
 		$(".edit-note").on("click", this.editNote.bind(this));
+		$(".update-note").on("click", this.updateNote.bind(this));
 	}
 
 	// CRUD Methods will go here
+
+	updateNote(e) {
+		var thisNote = $(e.target).parents("li");
+		var ourUpdatedPost = {
+			// very specific property names
+			'title': thisNote.find(".note-title-field").val(),
+			'content': thisNote.find(".note-body-field").val()
+		}
+
+		$.ajax({
+			beforeSend: (xhr) => {
+				xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+			},
+			url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),	// wp url where post json is showing when you use the data attribute in javascript, you don't need to specify data-id
+			type: 'POST',	// what type of request you want to send, GET POST DELETE
+			data: ourUpdatedPost,
+			success: (response) => {
+				this.makeNoteReadOnly(thisNote);
+				console.log('Congrats');
+				console.log(response);
+			},		// what you want to happen on success usually a function
+			error: (response) => {
+				console.log('Sorry...');
+				console.log(response);
+			}		// 
+		});	
+	}
 
 	editNote(e) {
 		var thisNote = $(e.target).parents("li");
--- wp-content/themes/fictional-university-theme/js/scripts-bundled.js
+++ wp-content/themes/fictional-university-theme/js/scripts-bundled.js
@@ -13778,8 +13778,44 @@ function () {
     value: function events() {
       (0, _jquery.default)(".delete-note").on("click", this.deleteNote);
       (0, _jquery.default)(".edit-note").on("click", this.editNote.bind(this));
+      (0, _jquery.default)(".update-note").on("click", this.updateNote.bind(this));
     } // CRUD Methods will go here
 
+  }, {
+    key: "updateNote",
+    value: function updateNote(e) {
+      var _this = this;
+
+      var thisNote = (0, _jquery.default)(e.target).parents("li");
+      var ourUpdatedPost = {
+        // very specific property names
+        'title': thisNote.find(".note-title-field").val(),
+        'content': thisNote.find(".note-body-field").val()
+      };
+
+      _jquery.default.ajax({
+        beforeSend: function beforeSend(xhr) {
+          xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+        },
+        url: universityData.root_url + '/wp-json/wp/v2/note/' + thisNote.data('id'),
+        // wp url where post json is showing when you use the data attribute in javascript, you don't need to specify data-id
+        type: 'POST',
+        // what type of request you want to send, GET POST DELETE
+        data: ourUpdatedPost,
+        success: function success(response) {
+          _this.makeNoteReadOnly(thisNote);
+
+          console.log('Congrats');
+          console.log(response);
+        },
+        // what you want to happen on success usually a function
+        error: function error(response) {
+          console.log('Sorry...');
+          console.log(response);
+        } // 
+
+      });
+    }
   }, {
     key: "editNote",
     value: function editNote(e) {

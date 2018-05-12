ea31ad65a15b8ff9ec845098824395995e56748f
Adam <adamf04@gmail.com>
2018-05-12 18:02:17 -0400

    ensures users cannot insert html into any part of the database
    you can never have too many layers of protection...

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -223,13 +223,19 @@ add_filter('login_headertitle', 'ourLoginTitle');
 /*--------------------------------------------------------------------------------------------------------------------------------*/
 
 /*******************************************/
-/*		Customize Notes to be private      */
+/*			Customize Notes.               */
 /*******************************************/
 
 // force note posts to be private
 add_filter('wp_insert_post_data', 'makeNotePrivate');
 
 function makeNotePrivate($data) {
+	// stop any post body to have any html injections
+	if ($data['post_type'] == 'note') {
+		$data['post_title'] = sanitize_text_field($data['post_title']);
+		$data['post_content'] = sanitize_textarea_field($data['post_content']);
+	}
+	// makes notes private
 	if ($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
  		$data['post_status'] = "private";
 	}
